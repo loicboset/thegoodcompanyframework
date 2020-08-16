4b57859b37baae645da82cfc8c37980ac2737438
Loic Boset <loic.boset@gmail.com>
2020-08-16 13:12:59 +0200

    Adding Devise gem for user authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_174944) do
-
-  create_table "ideas", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.integer "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_ideas_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2020_08_16_111054) do
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "ideas", "users"
 end
